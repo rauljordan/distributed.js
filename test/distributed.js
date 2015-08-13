e559e02f401d301962d7bef8f655dbe23837cbfb
Raul Jordan <raulitojordan@gmail.com>
2015-08-13 16:00:57 -0600

    added component creation to genbrain object

--- test/distributed.js
+++ test/distributed.js
@@ -1,23 +1,24 @@
-var distributedArchitecture = require('../');
 
+var genbrain = require('../');
 
-/**
- * Creates a basic architecture that is loggable
- * and adds some basic computations that will be active forever.
- * It logs the current state continuously, every 5 seconds.
- */
-var arc = distributedArchitecture({
-  loggable:true,
-  logTime:5
+function calculateTemperature() {
+  console.log(5);
+}
+
+function calculateHumidity() {
+  console.log(10);
+}
+
+var gb = genbrain({
+  loggable:false,
+  reactiveVars:'all'
 });
 
-arc.addComputations({
-  multiply: function (x) {
-    return x * 2;
-  },
-  addition:function (x) {
-    return x + 2;
-  }
+gb.components({
+  funcs:[
+    calculateHumidity,
+    calculateTemperature
+  ]
 });
 
-arc.begin();
+gb.start();
